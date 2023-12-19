f4d45cbbb1f273432704503bfe0d7a74f3ffaa91
Sacha Dubois <sdubois@vmware.com>
2023-12-19 08:29:09 +0100

    update newsletter-ui

--- newsletter-ui/src/app/subscriptions.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Subscriptions } from './subscriptions';
-
-describe('Subscriptions', () => {
-  it('should create an instance', () => {
-    expect(new Subscriptions()).toBeTruthy();
-  });
-});
