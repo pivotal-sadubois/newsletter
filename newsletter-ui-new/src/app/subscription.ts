e2712f95f5bc50b7432d2b0adc9e08c436cc2c78
Sacha Dubois <sdubois@vmware.com>
2023-12-19 12:56:35 +0100

    test

---
+++ newsletter-ui-new/src/app/subscription.ts
@@ -0,0 +1,13 @@
+export class Subscription {
+    id: number;
+    firstName: string;
+    lastName: string;
+    emailId: string;
+
+    constructor() {
+        this.id = 0;
+        this.firstName = '';
+        this.lastName = '';
+        this.emailId = '';
+      }
+}
---
+++ newsletter-ui-new/src/app/subscriptions.spec.ts
@@ -0,0 +1,7 @@
+import { Subscriptions } from './subscriptions';
+
+describe('Subscriptions', () => {
+  it('should create an instance', () => {
+    expect(new Subscriptions()).toBeTruthy();
+  });
+});
